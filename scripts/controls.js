6369e98a5b56626d5812e933719db5eff8142a4f
odpinerosh <odpinerosh@gmail.com>
2022-09-29 15:55:19 -0500

    Added Footer Section

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,11 +1,15 @@
-const btnLeft = document.getElementById("btnLeft");
-const btnRight = document.getElementById("btnRight");
-const tablesContainer = document.getElementById("tablesContainer");
+const btnLeft = document.getElementById('btnLeft');
+const btnRight = document.getElementById('btnRight');
+const tablesContainer = document.getElementById('tablesContainer');
+const footerYear = document.getElementById('footer-year');
+
 
 window.addEventListener('DOMContentLoaded', (event) => {
+    let today = new Date();
     tablesContainer.scrollLeft = 0;
     btnLeft.style.visibility = "hidden"
     btnRight.style.visibility = "visible"
+    footerYear.innerText = today.getFullYear();
 });
 
 btnLeft.addEventListener('click', () => {
